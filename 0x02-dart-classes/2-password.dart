0672d9e7044559ffda4b4a13e21f3a081efdc842
Erwan2072 <erwan.lebreton@hotmail.fr>
2025-07-15 15:25:01 +0200

    Fix regex for digit validation in Password class

--- 0x02-dart-classes/2-password.dart
+++ 0x02-dart-classes/2-password.dart
@@ -10,7 +10,7 @@ class Password {
     // Vérifier la présence de majuscules, minuscules, chiffres, caractère spécial, longueur du MDP
     bool hasUpper = password.contains(RegExp(r'[A-Z]'));
     bool hasLower = password.contains(RegExp(r'[a-z]'));
-    bool hasDigit = password.contains(RegExp(r'[0-9]'));
+    bool hasDigit = password.contains(RegExp(r'\d'));
     bool lengthValid = password.length >= 8 && password.length <= 16;
 
     return hasUpper && hasLower && hasDigit && lengthValid;
