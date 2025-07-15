394546b971620d54b0150dd9d13c6b8521402090
Erwan2072 <erwan.lebreton@hotmail.fr>
2025-07-15 15:23:35 +0200

    Add Password class with validation logic and toString method

---
+++ 0x02-dart-classes/2-password.dart
@@ -0,0 +1,22 @@
+class Password {
+  String password = "";
+
+  bool isValid() {
+    // Vérifier la longueur
+    if (password.length < 8 || password.length > 16) {
+      return false;
+    }
+
+    // Vérifier la présence de majuscules, minuscules, chiffres, caractère spécial, longueur du MDP
+    bool hasUpper = password.contains(RegExp(r'[A-Z]'));
+    bool hasLower = password.contains(RegExp(r'[a-z]'));
+    bool hasDigit = password.contains(RegExp(r'[0-9]'));
+    bool lengthValid = password.length >= 8 && password.length <= 16;
+
+    return hasUpper && hasLower && hasDigit && lengthValid;
+
+  @override
+  String toString() {
+    return "Your Password is: $password";
+  }
+}
