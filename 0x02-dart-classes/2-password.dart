fff360d1571ff2b251ce87cdd709d9e7ade71f7c
Erwan2072 <erwan.lebreton@hotmail.fr>
2025-07-15 15:28:27 +0200

    Refactor isValid method in Password class to streamline validation logic

--- 0x02-dart-classes/2-password.dart
+++ 0x02-dart-classes/2-password.dart
@@ -1,12 +1,9 @@
 class Password {
   String password = "";
 
-  bool isValid() {
-    // Vérifier la longueur
-    if (password.length < 8 || password.length > 16) {
-      return false;
-    }
+  Password()
 
+  bool isValid() { 
     // Vérifier la présence de majuscules, minuscules, chiffres, caractère spécial, longueur du MDP
     bool hasUppercase = password.contains(RegExp(r'[A-Z]'));
     bool hasLowercase = password.contains(RegExp(r'[a-z]'));
@@ -14,7 +11,8 @@ class Password {
     bool lengthValid = password.length >= 8 && password.length <= 16;
 
     return hasUpper && hasLower && hasDigit && lengthValid;
-
+  }
+  
   @override
   String toString() {
     return "Your Password is: $password";
