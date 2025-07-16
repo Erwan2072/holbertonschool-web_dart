83b559cd10e078fe6e5b11def2c43f014298d30e
Erwan2072 <erwan.lebreton@hotmail.fr>
2025-07-16 12:14:53 +0200

    Refactor printRmCharacters function for improved pagination handling and code clarity

--- 0x03-asynchronous/rick_and_morty.dart
+++ 0x03-asynchronous/rick_and_morty.dart
@@ -3,29 +3,20 @@ import 'package:http/http.dart' as http;
 
 Future<void> printRmCharacters() async {
   try {
-    int page = 1;
-    bool hasNextPage = true;
-
-    while (hasNextPage) {
-      final url = 'https://rickandmortyapi.com/api/character?page=$page';
+    String url = 'https://rickandmortyapi.com/api/character';
+    while (url.isNotEmpty) {
       final response = await http.get(Uri.parse(url));
-
       if (response.statusCode == 200) {
         final data = jsonDecode(response.body);
-        final List<dynamic> characters = data['results'];
-
-        for (var character in characters) {
+        for (final character in data['results']) {
           print(character['name']);
         }
-
-
-        hasNextPage = data['info']['next'] != null;
-        page++;
+        url = data['info']['next'] ?? '';
       } else {
-        throw Exception('Failed to load characters. Status: ${response.statusCode}');
+        throw Exception('Failed to load characters');
       }
     }
-  } catch (error) {
-    print('error caught: $error');
+  } catch (e) {
+    print('error caught: $e');
   }
 }
